0c62634a8db0e823d23442c91459141107626114
manhldvxut <manhldvxut@gmail.com>
2020-09-27 23:55:28 +0900

    complete

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './components/Nav/Navbar';
+import Navbar2 from './components/Navbar2';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
+
+function App() {
+  return (
+    <div className="App">
+
+    <Router>
+        <Navbar2 />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
+      
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
